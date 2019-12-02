412b871a4fdaa6d17a1a8b8cf6d91b03a2031d93
Arch-vile <mikko.ravimo@gmail.com>
2019-12-02 22:03:17 +0200

    Pool counting works with the simplest case

--- mission003/main.kt
+++ mission003/main.kt
@@ -1,25 +1,41 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.io.File
+import java.lang.RuntimeException
+import kotlin.math.max
 
 //DEPS com.fasterxml.jackson.core:jackson-core:2.10.1, com.fasterxml.jackson.core:jackson-databind:2.10.1, com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1
 
 fun main(args: Array<String>) {
+	val testData = listOf(2,1,0,-2,0,-1,0,1,3)
 
-	var data =
-			ObjectMapper()
-					.registerModule(KotlinModule())
-					.readValue(File(args[0]), FloodData::class.java)
+	var poolVolume: Int = calculatePools(testData)
+	var expected = 15
+	if (poolVolume != expected)
+		throw RuntimeException("Wrong volume, got $poolVolume expected $expected")
 
+	println("All good!!!")
 
-	data.regions
-			.map { it.regionID }
-			.forEachIndexed { index, s ->   println("$index $s") }
-
+//	var data =
+//			ObjectMapper()
+//					.registerModule(KotlinModule())
+//					.readValue(File(args[0]), FloodData::class.java)
 
 
 }
 
+
+// TODO: remember to skip the start and end slopes
+// TODO: always towards highest point (from left / right)
+fun calculatePools(data: List<Int>): Int {
+	var sum = 0
+	var currentReferenceHeight = data[0]
+	for (height in data) {
+		currentReferenceHeight = max(currentReferenceHeight, height)
+		sum += currentReferenceHeight - height
+	}
+	return sum
+}
 data class FloodData(
 		val regions: List<Region>
 )
