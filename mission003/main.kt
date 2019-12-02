3e3c4d2932a9862a151a2773aae7fdc78984d962
Arch-vile <mikko.ravimo@gmail.com>
2019-12-02 21:41:09 +0200

    TEMP

--- mission003/main.kt
+++ mission003/main.kt
@@ -1,6 +1,36 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import java.io.File
+
+//DEPS com.fasterxml.jackson.core:jackson-core:2.10.1, com.fasterxml.jackson.core:jackson-databind:2.10.1, com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1
+
 fun main(args: Array<String>) {
 
-	println("hello world")
+	var data =
+			ObjectMapper()
+					.registerModule(KotlinModule())
+					.readValue(File(args[0]), FloodData::class.java)
+
+
+	data.regions
+			.map { it.regionID }
+			.forEachIndexed { index, s ->   println("$index $s") }
+
+
 
 }
 
+data class FloodData(
+		val regions: List<Region>
+)
+
+data class Region(
+		val regionID: String,
+		val readings: List<Reading>
+)
+
+data class Reading(
+		val readingID: String,
+		val date: String,
+		val reading: List<Int>
+)
