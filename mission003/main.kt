a1899f4f565f2025760ae857afb2cd858304d595
Arch-vile <mikko.ravimo@gmail.com>
2019-12-03 21:18:14 +0200

    Works by dividing the area by max

--- mission003/main.kt
+++ mission003/main.kt
@@ -1,20 +1,28 @@
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
-import java.io.File
-import java.lang.RuntimeException
 import kotlin.math.max
 
 //DEPS com.fasterxml.jackson.core:jackson-core:2.10.1, com.fasterxml.jackson.core:jackson-databind:2.10.1, com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1
 
 fun main(args: Array<String>) {
-	val testData = listOf(2,1,0,-2,0,-1,0,1,3)
+    testPoolFunction(
+            listOf(2, 1, 0, -2, 0, -1, 0, 1, 4),
+            15)
 
-	var poolVolume: Int = calculatePools(testData)
-	var expected = 15
-	if (poolVolume != expected)
-		throw RuntimeException("Wrong volume, got $poolVolume expected $expected")
+    testPoolFunction(
+            listOf(2, 1, 0, -2, 0, -1, 0, 1, 4, 1, 0, -2, 0, 4),
+            32)
 
-	println("All good!!!")
+    // TODO fix this
+    testPoolFunction(
+            listOf(2, 1, 0, -2, 0, -1, 0, 1, 4, 1, 0, -2, 0, 2),
+            24)
+
+
+    // Missing test: multiple maximums on the data
+    // Missing test: max is first or last value
+
+
+
+    println("All good!!!")
 
 //	var data =
 //			ObjectMapper()
@@ -24,29 +32,45 @@ fun main(args: Array<String>) {
 
 }
 
+private fun testPoolFunction(testData: List<Int>, expected: Int) {
+    var poolVolume: Int = calculatePools(testData)
+    if (poolVolume != expected)
+        throw RuntimeException("Wrong volume, got $poolVolume expected $expected")
+}
 
 // TODO: remember to skip the start and end slopes
 // TODO: always towards highest point (from left / right)
 fun calculatePools(data: List<Int>): Int {
-	var sum = 0
-	var currentReferenceHeight = data[0]
-	for (height in data) {
-		currentReferenceHeight = max(currentReferenceHeight, height)
-		sum += currentReferenceHeight - height
-	}
-	return sum
+    var maxHeight = data.max()
+    var indexOfMax = data.indexOfFirst { it == maxHeight }
+
+    var leftOfMax = data.subList(0, indexOfMax + 1)
+    var rightOfMax = data.subList(indexOfMax, data.size).reversed()
+
+    return calculateSection(leftOfMax) + calculateSection(rightOfMax)
 }
+
+fun calculateSection(data: List<Int>): Int {
+    var sum = 0
+    var currentReferenceHeight = data[0]
+    for (height in data) {
+        currentReferenceHeight = max(currentReferenceHeight, height)
+        sum += currentReferenceHeight - height
+    }
+    return sum
+}
+
 data class FloodData(
-		val regions: List<Region>
+        val regions: List<Region>
 )
 
 data class Region(
-		val regionID: String,
-		val readings: List<Reading>
+        val regionID: String,
+        val readings: List<Reading>
 )
 
 data class Reading(
-		val readingID: String,
-		val date: String,
-		val reading: List<Int>
+        val readingID: String,
+        val date: String,
+        val reading: List<Int>
 )
