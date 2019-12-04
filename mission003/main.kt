9b35dd748e7cbfa6b36e807d9f656521ca9c6c39
Arch-vile <mikko.ravimo@gmail.com>
2019-12-04 20:34:03 +0200

    TEMP

--- mission003/main.kt
+++ mission003/main.kt
@@ -3,6 +3,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Comparator
+import kotlin.math.abs
 import kotlin.math.max
 
 //DEPS com.fasterxml.jackson.core:jackson-core:2.10.1, com.fasterxml.jackson.core:jackson-databind:2.10.1, com.fasterxml.jackson.module:jackson-module-kotlin:2.10.1
@@ -14,44 +15,38 @@ fun main(args: Array<String>) {
                     .registerModule(KotlinModule())
                     .readValue(File(args[0]), FloodData::class.java)
 
-    data.regions.forEach {
+
+    data.regions.map {
         checkRegion(it)
     }
-
-
+            .flatten()
+            .sortedWith(Comparator { r1, r2 ->
+                toDate(r1.second.second).compareTo(toDate(r2.second.third))
+            })
+            .forEach {
+                val region = it.first
+                val delta = it.second.first
+                val firstDay = it.second.second
+                val secondDay = it.second.third
+                println("Region ${region.regionID} delta $delta first ${firstDay.readingID}  ${firstDay.date}\t second ${secondDay.readingID} ${secondDay.date}")
+            }
 }
 
-private fun checkRegion(region: Region) {
+private fun checkRegion(region: Region): List<Pair<Region, Triple<Int, Reading, Reading>>> {
     var sorted = region.readings
             .sortedWith(Comparator { r1, r2 ->
                 toDate(r1).compareTo(toDate(r2))
             })
 
-    val suspicious = sorted.windowed(2, 1)
+    return sorted.windowed(2, 1)
             .map {
                 val firstDay = it[0]
                 val secondDay = it[1]
                 val delta = calculatePools(secondDay.reading) - calculatePools(firstDay.reading)
-                var triple = Triple(delta, firstDay, secondDay)
-                println(triple)
-                triple
+                Pair(region, Triple(delta, firstDay, secondDay))
             }
-            .filter { it.first > 1000 }
+            .filter { abs(it.second.first) > 100 }
 
-//    val numbers = suspicious
-//            .map { it.first }
-//    println(numbers[0])
-
-//    numbers
-//            .sortedBy { it  }
-//            .forEach { println(it) }
-//            .sortedBy { triple -> triple.first }
-//            .forEach {
-//                val delta = it.first
-//                val firstDay = it.second
-//                val secondDay = it.third
-//                println("Region ${region.regionID} delta $delta first ${firstDay.readingID}  ${firstDay.date}\t second ${secondDay.readingID} ${secondDay.date}")
-//            }
 }
 
 fun toDate(r1: Reading) =
@@ -59,6 +54,7 @@ fun toDate(r1: Reading) =
 
 private fun testPoolFunction(testData: List<Int>, expected: Int) {
     var poolVolume: Int = calculatePools(testData)
+    println("Data: $testData Result: $poolVolume")
     if (poolVolume != expected)
         throw RuntimeException("Wrong volume, got $poolVolume expected $expected")
 }
@@ -99,7 +95,7 @@ data class Reading(
 )
 
 
-fun foo() {
+fun runTests() {
 
     testPoolFunction(
             listOf(2, 1, 0, -2, 0, -1, 0, 1, 4),
@@ -134,4 +130,16 @@ fun foo() {
     testPoolFunction(listOf(0, 1, 0), 0)
     testPoolFunction(listOf(0, 1, 2, 3, 2, 1), 0)
     testPoolFunction(listOf(0, 1, 2, 2, 2, 3, 5), 0)
+
+    // Timon testit
+    testPoolFunction(listOf(7, 7, 6, 5, 4, 4, 9, 8, 8, 7, 7, 7), 9)
+    testPoolFunction(listOf(2, 1, 2, 0, 1, 0, 3), 6)
+    testPoolFunction(listOf(2, 0, 1, 0, 1, 0, 3), 8)
+    testPoolFunction(listOf(2, 0, 0, 1), 2)
+    testPoolFunction(listOf(2, 0, 0, 4, 0, 0, 4), 12)
+    testPoolFunction(listOf(12, 0, 0, 4, 0, 0, 4), 16)
+    testPoolFunction(listOf(12, 0, 0, 4, 0, 0, 5), 21)
+    testPoolFunction(listOf(0, 12, 0, 3, 0, 0, 5), 17)
+    testPoolFunction(listOf(0, 12, 0, 12, 0, 0, 5), 22)
+
 }
